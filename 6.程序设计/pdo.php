4a8bd4370fa7ea15cdaad098f0e2a325fa3e82ad
gaopan <gaopan_vip@163.com>
2019-06-12 22:24:27 +0800

    mysql 必备

---
+++ 6.程序设计/pdo.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * PDO连接数据库
+ * */
+$dbms='mysql';     //数据库类型
+$host='localhost'; //数据库主机名
+$dbName='test';    //使用的数据库
+$user='root';      //数据库连接用户名
+$pass='';          //对应的密码
+$dsn="$dbms:host=$host;dbname=$dbName";
+
+
+try {
+    $dbh = new PDO($dsn, $user, $pass); //初始化一个PDO对象
+    echo "连接成功<br/>";
+    /*你还可以进行一次搜索操作
+    foreach ($dbh->query('SELECT * from FOO') as $row) {
+        print_r($row); //你可以用 echo($GLOBAL); 来看到这些值
+    }
+    */
+    $dbh = null;
+} catch (PDOException $e) {
+    die ("Error!: " . $e->getMessage() . "<br/>");
+}
+//默认这个不是长连接，如果需要数据库长连接，需要最后加一个参数：array(PDO::ATTR_PERSISTENT => true) 变成这样：
+$db = new PDO($dsn, $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+
+
+/*
+ * mysqli连接数据库
+ * */
+/* Connect to a MySQL server  连接数据库服务器 */
+$link = mysqli_connect(
+    'localhost',  /* The host to connect to 连接MySQL地址 */
+    'jian',      /* The user to connect as 连接MySQL用户名 */
+    '123456',  /* The password to use 连接MySQL密码 */
+    'jian');    /* The default database to query 连接数据库名称*/
+
+if (!$link) {
+    printf("Can't connect to MySQL Server. Errorcode: %s ", mysqli_connect_error());
+    exit;
+}else
+    echo '数据库连接上了！';
+
+/* Close the connection 关闭连接*/
+mysqli_close($link);
